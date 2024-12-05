f7c3a058b0cc84a4cae9074b687ba2f74e3c8cb7
Abubokkor98 <mail.abubokkor@gmail.com>
2024-12-05 17:10:00 +0600

    get only user uploaded equipment

--- index.js
+++ index.js
@@ -1,70 +1,70 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
-
 // mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4nvaj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 // function for mongodb
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const equipmentCollection = client.db('A10-DB').collection("euipments");
+    const equipmentCollection = client.db("A10-DB").collection("euipments");
 
     // get all equipments
-    app.get('/equipments',async(req,res)=>{
-        const cursor = equipmentCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/equipments", async (req, res) => {
+      const cursor = equipmentCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    // post euipment
-    app.post('/equipments',async(req,res)=>{
-        const newEquipment = req.body;
-        console.log(newEquipment);
-        const result = await equipmentCollection.insertOne(newEquipment);
+    // get only user uploaded equipment
+    app.get("/equipments/user", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      const result = await equipmentCollection.find({email}).toArray();
         res.send(result);
     });
 
-
-
-
+    // post euipment
+    app.post("/equipments", async (req, res) => {
+      const newEquipment = req.body;
+      const result = await equipmentCollection.insertOne(newEquipment);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } catch (error) {
-        console.log(error);
-    }
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.log(error);
+  }
 }
 run();
 
-
-
-app.get("/",(req,res)=>{
-res.send('Assignment-10 server is running')
+app.get("/", (req, res) => {
+  res.send("Assignment-10 server is running");
 });
 
-app.listen(port,()=>{
-    console.log(`Assignment-10 Server is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Assignment-10 Server is running on port: ${port}`);
+});
